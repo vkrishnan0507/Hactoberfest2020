737bac2cb5e978309f5d03b99411b23c713bf2a7
Vanitha K <59090053+vkrishnan0507@users.noreply.github.com>
2020-10-05 15:41:04 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,13 +1,12 @@
-#include <iostream>  
-using namespace std;  
+#include <iostream.h> 
 int main()  
 {  
    int i,fact=1,number;    
-  cout<<"Enter any Number: ";    
+  cout<<"Enter any Number, you want to know the factorial: ";    
  cin>>number;    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  cout<<"Factorial of " <<number<<" is: "<<fact<<endl;  
+  cout<<"Factorial of the number " <<number<<" you entered is: "<<fact<<endl;  
   return 0;  
 }  
